c1c9aadf69a7a88f77b0960fd0068f1cd127ce62
Jeno Csupor <jeno.csupor@endava.com>
2026-04-03 18:34:32 +0300

    Changed BatchSize to 100

--- scraper/Settings.cs
+++ scraper/Settings.cs
@@ -6,6 +6,6 @@
         public const string JsonFileName = "albums.json";
         public const string ThumbnailsFolder = "thumbnails";
         public const string ThumbnailQuality = "w1200-h800-p-k-no";
-        public const int BatchSize = 50;
+        public const int BatchSize = 100;
     }
 }
